b346ec12a8664042b6202cc2378373960e3a5d23
javafine <javafine@163.com>
2022-10-17 15:10:50 +0800

    remove invalid import code

--- dynamic-bean-demo/src/test/java/com/fine/demo/dynamicbean/controller/SampleControllerTest.java
+++ dynamic-bean-demo/src/test/java/com/fine/demo/dynamicbean/controller/SampleControllerTest.java
@@ -1,26 +1,17 @@
 package com.fine.demo.dynamicbean.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fine.demo.dynamicbean.controller.SampleController;
 import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 /**
  * SampleController Tester.
